06a8443d9f6b26be844918d497fc2eca98460f03
Tseng Po-Yen <allen050883@gmail.com>
2019-05-20 16:41:31 +0800

    Update octave_convolution.py

--- tensorflow/octave_convolution.py
+++ tensorflow/octave_convolution.py
@@ -1,28 +1,84 @@
 import tensorflow as tf
 
-def octave_conv(T, filter_num_out, kernel_size, alpha_in, alpha_out):
-    L_in = T[:,:,:,: int(alpha_in * int(T.shape[3]))]
-    L_in = tf.image.resize_images(L_in, (int(int(T.shape[1])/2), int(int(T.shape[2])/2)), method = 0)
-    
-    H_in = T[:,:,:, int(alpha_in * int(T.shape[3])):]
-    
-    #filter num of Low and High frequency
-    L_out = int(alpha_out * filter_num_out)
-    H_out = filter_num_out- int(alpha_out * filter_num_out)
-    
-    L2L = tf.layers.conv2d(L_in, L_out, kernel_size, 1, 'same')
-    H2H = tf.layers.conv2d(H_in, H_out, kernel_size, 1, 'same')
-    
-    L2H = tf.layers.conv2d_transpose(L_in, H_out, kernel_size, 2, 'same')
-    H2L = tf.layers.average_pooling2d(H_in, kernel_size, 2, 'same')
-    H2L = tf.layers.conv2d(H2L, L_out, kernel_size, 1, 'same')
-    
-    H = H2H + L2H
-    L = L2L + H2L
-    
-    return H, L
+def octave_conv(oct_type, H = 0, L = 0, filter_num_out = 2048, kernel_size = 5, kernel = tf.keras.initializers.lecun_normal(), alpha_in = 0.5, alpha_out = 0.5):
+    if oct_type == 'first':
+        alpha_in = 0
+        filter_num_out = filter_num_out * 2
+        H_in = H[:,:,:, int(alpha_in * int(H.shape[3])):]
+        
+        #filter num of Low and High frequency
+        L_out = int(alpha_out * filter_num_out)
+        H_out = filter_num_out- int(alpha_out * filter_num_out)
+        
+        H2H = tf.layers.conv2d(H_in, H_out, kernel_size, 1, 'same', kernel_initializer = kernel)
+        
+        H2L = tf.layers.average_pooling2d(H_in, kernel_size, 2, 'same')
+        H2L = tf.layers.conv2d(H2L, L_out, kernel_size, 1, 'same', kernel_initializer = kernel)
+        
+        H = H2H
+        L = H2L
+        
+        return H, L
 
-T = tf.zeros([10, 14, 14, 1024])
-H, L = octave_conv(T, 2048, 3, 0.5, 0.5)
-print(H.shape)
-print(L.shape)
+
+    elif oct_type == 'regular':
+        
+        filter_num_out = filter_num_out * 2
+        L_in = L
+        H_in = H
+        #filter num of Low and High frequency
+        L_out = int(alpha_out * filter_num_out)
+        H_out = filter_num_out- int(alpha_out * filter_num_out)
+        
+        L2L = tf.layers.conv2d(L_in, L_out, kernel_size, 1, 'same', kernel_initializer = kernel)
+        H2H = tf.layers.conv2d(H_in, H_out, kernel_size, 1, 'same', kernel_initializer = kernel)
+        
+        L2H = tf.layers.conv2d_transpose(L_in, H_out, kernel_size, 2, 'same', kernel_initializer = kernel)
+        H2L = tf.layers.average_pooling2d(H_in, kernel_size, 2, 'same')
+        H2L = tf.layers.conv2d(H2L, L_out, kernel_size, 1, 'same', kernel_initializer = kernel)
+        
+        H = H2H + L2H
+        L = L2L + H2L
+        
+        return H, L
+    
+    else:
+        alpha_out = 0
+        L_in = L
+        H_in = H
+        #filter num of Low and High frequency
+        L_out = int(alpha_out * filter_num_out)
+        H_out = filter_num_out- int(alpha_out * filter_num_out)
+        
+        L2L = tf.layers.conv2d(L_in, L_out, kernel_size, 1, 'same', kernel_initializer = kernel)
+        H2H = tf.layers.conv2d(H_in, H_out, kernel_size, 1, 'same', kernel_initializer = kernel)
+        
+        L2H = tf.layers.conv2d_transpose(L_in, H_out, kernel_size, 2, 'same', kernel_initializer = kernel)
+        H2L = tf.layers.average_pooling2d(H_in, kernel_size, 2, 'same')
+        H2L = tf.layers.conv2d(H2L, L_out, kernel_size, 1, 'same', kernel_initializer = kernel)
+        
+        H = H2H + L2H
+        L = L2L + H2L
+        
+        return H
+
+
+kernel = tf.keras.initializers.lecun_normal()
+img = tf.zeros([10, 2000, 3000, 1024])
+H, L = octave_conv('first', img, 0 , 2048, 5, kernel, 0, 0.5)
+print("first "); print("H shape:", H.shape); print("L shape:", L.shape)
+H, L = octave_conv('regular', H, L , 2048, 5, kernel, 0.5, 0.5)
+print("regular "); print("H shape:", H.shape); print("L shape:", L.shape)
+H = octave_conv('last', H, L , 2048, 5, kernel, 0.5, 0)
+print("last"); print("H shape:", H.shape)
+
+"""
+first 
+H shape: (10, 2000, 3000, 2048)
+L shape: (10, 1000, 1500, 2048)
+regular 
+H shape: (10, 2000, 3000, 2048)
+L shape: (10, 1000, 1500, 2048)
+last
+H shape: (10, 2000, 3000, 2048)
+""
