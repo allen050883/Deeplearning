b2677632528d9cbc02ea165b82399f90ad1bc376
Tseng Po-Yen <allen050883@gmail.com>
2019-06-17 15:00:14 +0800

    Update octave_convolution.py

--- tensorflow/octave_convolution.py
+++ tensorflow/octave_convolution.py
@@ -1,84 +1,40 @@
 import tensorflow as tf
 
-def octave_conv(oct_type, H = 0, L = 0, filter_num_out = 2048, kernel_size = 5, kernel = tf.keras.initializers.lecun_normal(), alpha_in = 0.5, alpha_out = 0.5):
+def octave_conv(oct_type, H, L, filter_num_out, kernel_size = 3, kernel = tf.keras.initializers.lecun_normal(), activation = tf.nn.selu, alpha = 0.5):
     if oct_type == 'first':
-        alpha_in = 0
-        filter_num_out = filter_num_out * 2
-        H_in = H[:,:,:, int(alpha_in * int(H.shape[3])):]
-        
         #filter num of Low and High frequency
-        L_out = int(alpha_out * filter_num_out)
-        H_out = filter_num_out- int(alpha_out * filter_num_out)
-        
-        H2H = tf.layers.conv2d(H_in, H_out, kernel_size, 1, 'same', kernel_initializer = kernel)
-        
-        H2L = tf.layers.average_pooling2d(H_in, kernel_size, 2, 'same')
-        H2L = tf.layers.conv2d(H2L, L_out, kernel_size, 1, 'same', kernel_initializer = kernel)
-        
-        H = H2H
-        L = H2L
-        
-        return H, L
-
+        L_out = int(alpha * filter_num_out // 1)
+        H_out = filter_num_out- int(alpha * filter_num_out)
+        H2H = tf.keras.layers.Conv2D(H_out, kernel_size, 1, 'same', kernel_initializer = kernel, activation = activation)(H)
+        H2L = tf.keras.layers.AveragePooling2D(2, 2, 'same')(H)
+        H2L = tf.keras.layers.Conv2D(L_out, kernel_size, 1, 'same', kernel_initializer = kernel, activation = activation)(H2L)
+        return H2H, H2L
 
     elif oct_type == 'regular':
-        
-        filter_num_out = filter_num_out * 2
-        L_in = L
-        H_in = H
         #filter num of Low and High frequency
-        L_out = int(alpha_out * filter_num_out)
-        H_out = filter_num_out- int(alpha_out * filter_num_out)
-        
-        L2L = tf.layers.conv2d(L_in, L_out, kernel_size, 1, 'same', kernel_initializer = kernel)
-        H2H = tf.layers.conv2d(H_in, H_out, kernel_size, 1, 'same', kernel_initializer = kernel)
-        
-        L2H = tf.layers.conv2d_transpose(L_in, H_out, kernel_size, 2, 'same', kernel_initializer = kernel)
-        H2L = tf.layers.average_pooling2d(H_in, kernel_size, 2, 'same')
-        H2L = tf.layers.conv2d(H2L, L_out, kernel_size, 1, 'same', kernel_initializer = kernel)
-        
-        H = H2H + L2H
-        L = L2L + H2L
-        
-        return H, L
+        L_out = int(alpha * filter_num_out // 1)
+        H_out = filter_num_out- int(alpha * filter_num_out)
+        H2H = tf.keras.layers.Conv2D(H_out, kernel_size, 1, 'same', kernel_initializer = kernel, activation=None)(H)
+        H2L = tf.keras.layers.AveragePooling2D(2, 2, 'same')(H)
+        H2L = tf.keras.layers.Conv2D(L_out, kernel_size, 1, 'same', kernel_initializer = kernel, activation=None)(H2L)
+        L2L = tf.keras.layers.Conv2D(L_out, kernel_size, 1, 'same', kernel_initializer = kernel, activation=None)(L)
+        L2H = tf.keras.layers.Conv2D(H_out, kernel_size, 1, 'same', kernel_initializer = kernel, activation=None)(L)
+        L2H = tf.keras.layers.UpSampling2D(interpolation='bilinear')(L2H)
+        return activation((H2H + L2H)/2), activation((L2L + H2L)/2)
     
     else:
-        alpha_out = 0
-        L_in = L
-        H_in = H
         #filter num of Low and High frequency
-        L_out = int(alpha_out * filter_num_out)
-        H_out = filter_num_out- int(alpha_out * filter_num_out)
-        
-        L2L = tf.layers.conv2d(L_in, L_out, kernel_size, 1, 'same', kernel_initializer = kernel)
-        H2H = tf.layers.conv2d(H_in, H_out, kernel_size, 1, 'same', kernel_initializer = kernel)
-        
-        L2H = tf.layers.conv2d_transpose(L_in, H_out, kernel_size, 2, 'same', kernel_initializer = kernel)
-        H2L = tf.layers.average_pooling2d(H_in, kernel_size, 2, 'same')
-        H2L = tf.layers.conv2d(H2L, L_out, kernel_size, 1, 'same', kernel_initializer = kernel)
-        
-        H = H2H + L2H
-        L = L2L + H2L
-        
-        return H
+        H2H = tf.keras.layers.Conv2D(filter_num_out, kernel_size, 1, 'same', kernel_initializer = kernel, activation=None)(H)
+        L2H = tf.keras.layers.Conv2D(filter_num_out, kernel_size, 1, 'same', kernel_initializer = kernel, activation=None)(L)
+        L2H = tf.keras.layers.UpSampling2D(interpolation='bilinear')(L2H)
+        return activation((H2H + L2H)/2)
 
 
 kernel = tf.keras.initializers.lecun_normal()
-img = tf.zeros([10, 2000, 3000, 1024])
-H, L = octave_conv('first', img, 0 , 2048, 5, kernel, 0, 0.5)
+img = tf.zeros([10, 32, 32, 3])
+H, L = octave_conv('first', img, 0 , 64, kernel_size = 3, alpha = 0.8)
 print("first "); print("H shape:", H.shape); print("L shape:", L.shape)
-H, L = octave_conv('regular', H, L , 2048, 5, kernel, 0.5, 0.5)
+H, L = octave_conv('regular', H, L , 128, kernel_size = 3, alpha = 0.8)
 print("regular "); print("H shape:", H.shape); print("L shape:", L.shape)
-H = octave_conv('last', H, L , 2048, 5, kernel, 0.5, 0)
+H = octave_conv('last', H, L , 256, kernel_size = 3, alpha = 0.8)
 print("last"); print("H shape:", H.shape)
-
-"""
-first 
-H shape: (10, 2000, 3000, 2048)
-L shape: (10, 1000, 1500, 2048)
-regular 
-H shape: (10, 2000, 3000, 2048)
-L shape: (10, 1000, 1500, 2048)
-last
-H shape: (10, 2000, 3000, 2048)
-""
