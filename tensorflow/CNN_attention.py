55e881d9c390cb9d55cc19cd87c1b31df367327d
Tseng Po-Yen <allen050883@gmail.com>
2019-07-01 16:30:39 +0800

    Update CNN_attention.py

--- tensorflow/CNN_attention.py
+++ tensorflow/CNN_attention.py
@@ -30,18 +30,35 @@ def Spatial_attention(Q, K, compression_channel_no = 16, name = 'satt'):
     return tf.reshape(attention, tf.shape(Q)) * tf.nn.sigmoid(gamma) + Q * tf.nn.sigmoid(1 - gamma) 
 
 
+def Channel_attention(Q, name = 'satt'):
+    
+    Qf = tf.reshape(Q, [-1, Q.shape[3].value, Q.shape[1].value * Q.shape[2].value])
+    Kf = tf.reshape(Q, [-1, Q.shape[3].value, Q.shape[1].value * Q.shape[2].value])
+    Vf = tf.reshape(Q, [-1, Q.shape[3].value,Q.shape[1].value * Q.shape[2].value])
+    
+    attention_map = tf.matmul(Qf, Kf, transpose_b=True)  # [bs, N, N]
+    attention_map = tf.nn.softmax(attention_map, axis=1) # consider only the keys for attention
+    attention = tf.matmul(attention_map, Vf)
+    
+    gamma = tf.get_variable(name+"satt_gamma", [1], initializer=tf.constant_initializer(0.0)) # set the gamma as learnable variable
+    return tf.reshape(attention, tf.shape(Q)) * tf.nn.sigmoid(gamma) + Q * tf.nn.sigmoid(1 - gamma) 
+
+
 # use difference source and target 
 S = tf.zeros([10, 28, 28, 3])
 T = tf.zeros([10, 14, 14, 1024])
 A = Attention(T, S, 'att1')
-print(A) # output should have the same shape of T (10, 14, 14, 1024)
+print('Attention1', A) # output should have the same shape of T (10, 14, 14, 1024)
 
 # puting self as two inputs will be self-attention
 A = Attention(T, T, 'att2')
-print(A)
+print('Attention2', A)
 
 #spatial attention
 S = tf.placeholder(tf.float32, [None,28,28,3])
 T = tf.placeholder(tf.float32, [None,10,10,1024])
 A = Spatial_attention(T,S) #(None, 10, 10, 1024)
-print(A)
+print('Spatial_attention', A)
+
+A = Channel_attention(S, 'att3'); print('Channel_attention1', A)
+A = Channel_attention(T, 'att4'); print('Channel_attention2', A)
