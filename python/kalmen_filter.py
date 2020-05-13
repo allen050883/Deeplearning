5517627a4278718a0dc8b5fb55caf0fdc6ced090
Tseng Po-Yen <allen050883@gmail.com>
2020-05-13 18:56:16 +0800

    Create kalmen_filter.py

---
+++ python/kalmen_filter.py
@@ -0,0 +1,57 @@
+import numpy as np
+from numpy.linalg import inv
+from numpy import dot, sum, tile, linalg
+
+def kf_predict(X, P, A, Q, B, U):
+    X = dot(A, X) + dot(B, U)
+    P = dot(A, dot(P, A.T)) + Q
+    return(X,P) 
+
+def kf_update(X, P, Y, H, R):
+    IM = dot(H, X)
+    IS = R + dot(H, dot(P, H.T))
+    K = dot(P, dot(H.T, inv(IS)))
+    X = X + dot(K, (Y-IM))
+    P = P - dot(K, dot(IS, K.T))
+    LH = gauss_pdf(Y, IM, IS)
+    return (X,P,K,IM,IS,LH)
+
+def gauss_pdf(X, M, S):
+    if M.shape[1] == 1:
+        DX = X - tile(M, X.shape[1])
+        E = 0.5 * sum(DX * (dot(inv(S), DX)), axis=0)
+        E = E + 0.5 * M.shape[0] * np.log(2 * np.pi) + 0.5 * np.log(np.linalg.det(S))
+        P = np.exp(-E)
+    elif X.shape[1] == 1:
+        DX = tile(X, M.shape[1])- M
+        E = 0.5 * sum(DX * (dot(inv(S), DX)), axis=0)
+        E = E + 0.5 * M.shape[0] * np.log(2 * np.pi) + 0.5 * np.log(np.linalg.det(S))
+        P = np.exp(-E)
+    else:
+        DX = X-M
+        E = 0.5 * dot(DX.T, dot(inv(S), DX))
+        E = E + 0.5 * M.shape[0] * np.log(2 * np.pi) + 0.5 * np.log(np.linalg.det(S))
+        P = np.exp(-E)
+    return (P[0],E[0]) 
+
+#time step of mobile movement
+dt = 0.1
+# Initialization of state matrices
+X = np.array([[0.0], [0.0], [0.1], [0.1]])
+P = np.diag((0.01, 0.01, 0.01, 0.01))
+A = np.array([[1, 0, dt , 0], [0, 1, 0, dt], [0, 0, 1, 0], [0, 0, 0, 1]])
+Q = np.eye(X.shape[0])
+B = np.eye(X.shape[0])
+U = np.zeros((X.shape[0],1))
+
+Y = np.array([[X[0,0] + np.abs(np.random.randn(1)[0])], [X[1,0] + np.abs(np.random.randn(1)[0])]])
+H = np.array([[1, 0, 0, 0], [0, 1, 0, 0]])
+R = np.eye(Y.shape[0])
+# Number of iterations in Kalman Filter
+N_iter = 50
+# Applying the Kalman Filter
+for i in np.arange(0, N_iter):
+    (X, P) = kf_predict(X, P, A, Q, B, U)
+    (X, P, K, IM, IS, LH) = kf_update(X, P, Y, H, R)
+    Y = np.array([[X[0,0] + np.abs(0.1 * np.random.randn(1)[0])],[X[1, 0] + np.abs(0.1 * np.random.randn(1)[0])]]) 
+    
